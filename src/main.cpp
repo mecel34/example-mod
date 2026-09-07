9e70450e3d7a760dba749167cce560edf7070d07
mecel34 <m.e.celik.2013@gmail.com>
2026-09-07 17:34:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,51 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class $modify(MyPlayLayer, PlayLayer) {
+    // 1. CHECKPOINT ALINDIĞINDA (SAVE)
+    void checkpointActivated(CheckpointObject* checkpoint) {
+        PlayLayer::checkpointActivated(checkpoint);
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+        if (Mod::get()->getSettingValue<bool>("auto-save") && this->m_isPlatformer) {
+            int levelID = this->m_level->m_levelID;
+            
+            // Kaçıncı checkpoint'te olduğumuzu ve o anki süreyi (time) al
+            size_t checkpointCount = this->m_checkpointArray->count();
+            double time = this->m_time;
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+            // Geode Kalıcı Hafızasına Kaydet
+            std::string keyCount = "save_count_" + std::to_string(levelID);
+            std::string keyTime = "save_time_" + std::to_string(levelID);
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+            Mod::get()->setSavedValue(keyCount, static_cast<int64_t>(checkpointCount));
+            Mod::get()->setSavedValue(keyTime, time);
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+            log::info("Platformer Save: Checkpoint #{} ve Süre ({}) kaydedildi!", checkpointCount, time);
+        }
+    }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+    // 2. SEVİYE YÜKLENDİĞİNDE VEYA YENİDEN BAŞLADIĞINDA (LOAD)
+    void resetLevel() {
+        PlayLayer::resetLevel();
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        if (Mod::get()->getSettingValue<bool>("auto-save") && this->m_isPlatformer) {
+            int levelID = this->m_level->m_levelID;
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+            std::string keyCount = "save_count_" + std::to_string(levelID);
+            std::string keyTime = "save_time_" + std::to_string(levelID);
+
+            // Kayıtlı veri var mı kontrol et
+            int64_t savedCount = Mod::get()->getSavedValue<int64_t>(keyCount, 0);
+            double savedTime = Mod::get()->getSavedValue<double>(keyTime, 0.0);
+
+            if (savedCount > 0) {
+                // Kayıtlı süreye ayarla
+                this->m_time = savedTime;
+
+                log::info("Platformer Load: Checkpoint verileri yuklendi! Checkpoint Sayisi: {}, Sure: {}", savedCount, savedTime);
+            }
+        }
+    }
 };
